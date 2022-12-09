9a57ae860677a0330c36868f139bebae189cf47d
swd19485 <119513136+swd19485@users.noreply.github.com>
2022-12-09 10:41:21 -0500

    Started Player Movement

---
+++ Assets/Scripts/PlayerStates/PlayerBaseState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBaseState : State
+{
+    protected PlayerStateMachine stateMachine;
+    protected void Move(Vector3 motion, float deltaTime)
+    {
+        stateMachine.Controller.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+
+    public PlayerBaseState(PlayerStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+}
