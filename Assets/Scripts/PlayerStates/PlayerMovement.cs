8c6bc87d85cf392e38f62f9cc3b339eb372bedec
swd19485 <119513136+swd19485@users.noreply.github.com>
2023-01-13 10:23:18 -0500

    f
    f

--- Assets/Scripts/PlayerStates/PlayerMovement.cs
+++ Assets/Scripts/PlayerStates/PlayerMovement.cs
@@ -5,32 +5,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
+    
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
- 
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
-
     }
 
     void Run()
     {
-        myRigidbody.velocity = moveInput;
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
 
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x + 1), 1f);
+        }
     }
 }
+
